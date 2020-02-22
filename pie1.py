3617fa55218fe782d5b1a420a6275992fde9d2a4
Han Shaoqing <502466541@qq.com>
2020-02-22 16:19:09 +0800

    Update pie1.py

--- pie1.py
+++ pie1.py
@@ -5,7 +5,7 @@ labels = '5’UTR', 'Others', '3’UTR', 'CDS'
 sizes = [15, 30, 45, 30]
 
 explode = (0.06,0.06,0.05, 0.05)  # only "explode" the 2nd slice (i.e. 'Hogs')
-colors = ["#CC7033","#C4573C","#4474BB","#6BA25E"]
+colors = ["#CC7033","#C4573C","#6BA25E","#4474BB"]
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         colors=colors, startangle=90)
